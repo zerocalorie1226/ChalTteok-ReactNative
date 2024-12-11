5afa0253284e9c2431749ca19cb6a61b0893d77e
조성찬 <kjyy07@naver.com>
2024-12-11 20:50:22 +0900

    [Design] 검색창 디자인

--- chaltteok_FE/src/components/Around/AroundContainer.tsx
+++ chaltteok_FE/src/components/Around/AroundContainer.tsx
@@ -1,22 +1,43 @@
 import React from 'react';
+import { Image } from 'react-native';
 import styled from 'styled-components/native';
+import searchIcon from '../../assets/Around/search.png';
 
 interface AroundContainerProps {
   text: string;
 }
 
 const AroundContainer: React.FC<AroundContainerProps> = ({ text }) => {
-  return <StyledInput placeholder={text} />;
+  return (
+    <>
+    <SearchContainer>
+      <StyledInput placeholder={text}  placeholderTextColor="#CCCCCC"/>
+      <SearchIcon source={searchIcon} />
+    </SearchContainer>
+    </>
+  );
 };
 
 export default AroundContainer;
+
+const SearchContainer=styled.View`
+  position:relative
+`
+
 
 const StyledInput = styled.TextInput`
   width: 343px;
   height: 48px;
   border: 1px solid #ccc;
   border-radius: 10px;
-  padding: 8px 16px;
+  padding: 13px 0 13px 42px;
   font-size: 14px;
   background-color: #fff;
+  
 `;
+
+const SearchIcon = styled.Image`
+  position: absolute;
+  top: 14px;
+  left: 16px;
+`
