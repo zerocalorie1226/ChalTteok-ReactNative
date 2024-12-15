3b6074ea02a37ae28087f65f937e12d1e69de4f9
조성찬 <kjyy07@naver.com>
2024-12-15 21:27:46 +0900

    [style] StorePreviewSection 스타일링

--- chaltteok_FE/src/components/Around/Around.tsx
+++ chaltteok_FE/src/components/Around/Around.tsx
@@ -6,9 +6,10 @@ import Map from '../Map/Map';
 // 이미지 경로
 const leftArrow = require('../../assets/Around/left.png');
 const downArrowBlack = require('../../assets/Around/downBlack.png');
-const LikeIcon= require('../../assets/Around/heart.png')
-const ScoreIcon=require('../../assets/Around/rating.png')
+const LikeIcon = require('../../assets/Around/heart.png')
+const ScoreIcon = require('../../assets/Around/rating.png')
 const downArrowGray = require('../../assets/Around/downGray.png');
+const emptyImage = require('../../assets/Around/img_empty.png')
 
 const longText = "이 텍스트는 30자 이상으로 작성된 예시 문장입니다. 초과된 부분은 말줄임표로 표시됩니다. 확인해보세요";
 
@@ -18,6 +19,14 @@ const truncatedText = longText.length > 30
   : longText;
 
 const Around: React.FC = () => {
+  const images = [
+    emptyImage,
+    emptyImage,
+    emptyImage,
+    emptyImage,
+    emptyImage,
+    emptyImage
+  ];
   return (
     <Container>
       {/* Header */}
@@ -63,7 +72,14 @@ const Around: React.FC = () => {
       <MiddleLine></MiddleLine>
 
       <StorePreviewImagesSection>
-        
+        <StorePreviewImagesContainer horizontal showsHorizontalScrollIndicator={false}>
+          {images.map((image, index) => (
+            <StorePreviewImages 
+              key={index} 
+              source={image} 
+            />
+          ))}
+        </StorePreviewImagesContainer>
       </StorePreviewImagesSection>
       </StorePreviewSection>
 
@@ -128,34 +144,38 @@ const FilterButton = styled.TouchableOpacity`
 const FilterText = styled.Text`
   font-size: 14px;
   font-weight: 500;
-  margin-right: 4px;
+  margin-right: 2px;
+  padding-left: 8px;
 `;
 
 const StorePreviewSection = styled.View`
-  margin: 0 auto;
   width: 343px;
   height: 237px;
   border-radius: 12px;
-  background-color: skyblue;
-  position: relative;
+  position: absolute; /* 부모 요소 기준으로 절대 위치 지정 */
+  left: 6%; /* 부모의 절반 위치 */
+  bottom: 5%; /* 화면 하단에서 5% 위로 띄움 */
+  z-index: 3; /* 다른 요소 위에 표시 */
+  background-color: #fff; 
+  shadow-color: #000; 
+  shadow-offset: 0px 4px; 
+  shadow-opacity: 0.1; 
+  shadow-radius: 10px; 
+  elevation: 5; /* 안드로이드 그림자 효과 */
 `;
+
 
 const StorePreviewIntriSection = styled.View`
   width: 343px;
   height: 108px;
-  background-color: yellow;
 `;
 
-const StorePreviewImagesSection = styled.View`
-  width: 343px;
-  height: 128px;
-  background-color: yellowgreen;
-`;
+
 
 const MiddleLine=styled.View`
-  left: 8px;
   width: 343px;
   height: 1px;
+  background-color: #EAEAEA;
 `;
 
 const StoreTitle=styled.Text`
@@ -223,3 +243,29 @@ const StoreAddressShortIcon=styled.Image`
   top: -53px;
   left: 133px;
 `
+
+const StorePreviewImagesSection = styled.View`
+  width: 343px;
+  height: 128px;
+
+
+`;
+
+const StorePreviewImagesContainer=styled.View`
+  width: 323px;
+  height: 88px;
+  flex-direction: row;
+
+  overflow: hidden; /* 넘치는 이미지 숨기기 */
+  position: relative;
+  top:20px;
+  left:20px;
+`
+
+const StorePreviewImages = styled.Image`
+width: 88px;
+height: 88px;
+border-radius: 6px;
+margin-right: 8px;
+`
+
